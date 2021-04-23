1326f1c151c0c3fd955f3cf63d018025075cd7eb
Ivan <bboymoroz@mail.ru>
2021-04-23 16:32:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+
 def display_board(board)
   puts "#{board[0]} | #{board[1]} | #{board[2]}"
   puts "-----------"
@@ -8,4 +8,4 @@ def display_board(board)
   puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]}"
 end
-display_board(board)
+
