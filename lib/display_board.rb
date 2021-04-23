55bbcf2abe64e451973f142fa81dfbf59c143c17
Ivan <bboymoroz@mail.ru>
2021-04-23 16:31:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,12 +2,10 @@
 # out the current state.
 board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 def display_board(board)
-  board.each do
-    puts "#{board[0]} | #{board[1]} | #{board[2]}"
-    puts "-----------"
-    puts "#{board[3]} | #{board[4]} | #{board[5]}"
-    puts "-----------"
-    puts "#{board[6]} | #{board[7]} | #{board[8]}"
-  end
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
 end
 display_board(board)
