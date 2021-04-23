aa909668771d21a65fb4d87c01efbd65241dbb89
Ivan <bboymoroz@mail.ru>
2021-04-23 16:25:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+def display_board(board)
+  i = 0
+  while i < board.size
+    if i = 2
+      
+  end
+end
